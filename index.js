c650d6579441821c45488cb48b7f55616261f3ec
jgretsky <105802424+jgretsky@users.noreply.github.com>
2023-01-23 18:54:14 -0500

    javascript click button

---
+++ index.js
@@ -0,0 +1,6 @@
+function myFunction()
+{
+    document.getElementById("demo").innerHTML = "Thank you for downloading my resume";
+}
+console.log("Your index.js file is loaded correctly!");
+console.log("Hello world!");
