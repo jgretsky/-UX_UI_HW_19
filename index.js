1813562e78a1e5609b887791d291e16c8a9b9f86
jgretsky <105802424+jgretsky@users.noreply.github.com>
2023-01-24 17:06:21 -0500

    updates

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 function myFunction()
 {
+    
     document.getElementById("demo").innerHTML = "Let's Go!";
 }
-console.log("Your index.js file is loaded correctly!");
-console.log("Hello world!");
+
+
+
+
+
+  /*$(".b_content").hover(function Blink() {
+    setInterval(Blink, 2000);
+
+    $(".circle").fadeOut(1000);
+    $(".circle").fadeIn(1000);
+  })
+*/
+
+  /*
+  function Blink() {
+    $(".circle").fadeOut(1000);
+    $(".circle").fadeIn(1000);
+  }
+
+  setInterval(Blink, 2000); */
