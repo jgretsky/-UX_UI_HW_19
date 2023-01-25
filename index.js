8104042ed1a2d5059ec03c175b18539a41d9e390
jgretsky <105802424+jgretsky@users.noreply.github.com>
2023-01-24 21:10:48 -0500

    blink

--- index.js
+++ index.js
@@ -8,13 +8,16 @@ function myFunction()
 
 
 
-  /*$(".b_content").hover(function Blink() {
+  $(".b_content").hover(function Blink() {
+
+     /* console.log("hover"); - used this to test to see if function is working*/
+
     setInterval(Blink, 2000);
 
     $(".circle").fadeOut(1000);
     $(".circle").fadeIn(1000);
   })
-*/
+
 
   /*
   function Blink() {
@@ -22,4 +25,29 @@ function myFunction()
     $(".circle").fadeIn(1000);
   }
 
-  setInterval(Blink, 2000); */
+  setInterval(Blink, 2000);
+  
+  example for blink:
+
+  (document).ready(function(){
+var g = $('.jumping');
+function blink(){
+  g.animate({ 'left':'50px' 
+  }).animate({
+     'left':'20px'
+        },1000)
+}
+setInterval(blink,1500);
+});
+
+example for blink 2: 
+
+function fadeContent() {
+  $(".content:hidden​​​​​​​​​​​​​​​​​​​​​​​:first").fadeIn(500).delay(2000).fadeOut(500, function​​​​​() {
+      $(this).appendTo($(this).parent()); //stick current at the end
+      fadeContent(); //loop
+  });
+}
+fadeContent(); //kick it off the first time
+
+*/
